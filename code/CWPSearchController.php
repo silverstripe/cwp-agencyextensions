4881a160b0a6ca5d8d9f984bb6a69a32d710275e
Simon Erkelens <simonerkelens@silverstripe.com>
2017-01-25 10:48:32 +1300

    CWPT-40 Search
    Extend the Page so we have the menu available. Renamed the search variable

---
+++ code/CWPSearchController.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ *
+ * @property CWPSearchController dataRecord
+ * @method CWPSearchController data()
+ * @mixin CWPSearchController dataRecord
+ */
+class CWPSearchController extends Page_Controller
+{
+    private static $page_limit = 10;
+
+    private static $allowed_actions = [
+    ];
+
+    public function index(SS_HTTPRequest $request)
+    {
+        $list = ArrayList::create();
+
+        $limit = static::config()->get('page_limit');
+        $total = 0;
+
+        if ($search = $request->getVar('Search')) {
+            $query = SearchQuery::create();
+            $query->search($search);
+
+            $offset = $request->getVar('start');
+
+            if (!$offset) {
+                $offset = 0;
+            }
+
+            /** @var ArrayData $results */
+            $results = Injector::inst()->get('CWPIndexer')->search($query, $offset, $limit);
+
+            /** @var PaginatedList $list */
+            $list = $results->Matches;
+
+            $limit = $list->getPageLength();
+            $total = $list->getTotalItems();
+        }
+
+        $data = ArrayData::create([
+            'Results' => $list,
+            'Limit'   => min($limit, $total),
+            'Total'   => $total,
+        ]);
+        return $this->renderWith(['Page_results', 'Page'], $data);
+    }
+
+    public function getTitle()
+    {
+        $title = 'Search ';
+        $title .= $this->request->getVar('Search')?: '';
+        return $title;
+    }
+
+    public function getSearchQuery()
+    {
+        return $this->request->getVar('Search');
+    }
+}
--- readme.md
+++ readme.md
@@ -6,3 +6,24 @@ This module comes with a githook you can install in your git repository, to keep
 Simply copy `githooks\pre-commit.php` to `.git\hooks\pre-commit` and it will lint your code on commit.
 
 If any error is found, the commit will exit and you will have to fix the code.
+
+CWP Search
+==========
+
+Search has it's own form and search index. To override the search index used, you can create your own and add the following to your config.yml:
+```
+Injector:
+  CWPIndexer:
+    class: CWPModuleSearchIndex
+```
+And replace CWPModuleSearchIndex with your own Index
+
+
+To change the amount of search results per page, add the following to your config:
+```
+CWPSearchController:
+  page_limit: 10
+
+```
+
+Where the limit is changeable to whatever limit you prefer.
